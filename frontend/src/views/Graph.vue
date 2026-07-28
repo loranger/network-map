cceb066decda34eea011f635f414260beb58461f
Laurent Goussard <loranger@free.fr>
2026-07-28 21:33:52 +0200

    Revert "feat: reduce padding for single-device location compound nodes"
    This reverts commit 00a667dd6b347a4a751fe1ba1026c8b7265c6bce.

--- frontend/src/views/Graph.vue
+++ frontend/src/views/Graph.vue
@@ -248,13 +248,6 @@ function buildGraph(data) {
 
   cy.add(elements)
 
-  cy.nodes('node.location').forEach(loc => {
-    const children = loc.children('node:childless')
-    if (children.length === 1) {
-      loc.style('padding', '8px')
-    }
-  })
-
   const layout = cy.layout({
     name: 'fcose',
     animate: false,
